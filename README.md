bfcd612457c06b3f4c8d23960b1393229a8fc0e6
hrubanj <jiri.hruban@email.cz>
2019-11-11 13:16:05 +0100

    Added template files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # keboola_component_template
+
+Put your code to src/main.py
---
+++ src/main.py
@@ -0,0 +1,2 @@
+from keboola import docker
+# install via: pip3 install https://github.com/keboola/python-docker-application/zipball/master
